972e50a332931a5384ebf937301a333135da71b9
Jared Grippe <jared@deadlyicon.com>
2023-01-19 12:52:13 -0800

    added logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url'
 import Path from 'path'
 import express from 'express'
+import morgan from 'morgan'
 import Gun from 'gun'
 import dotenv from 'dotenv'
 
@@ -13,6 +14,31 @@ process.on('unhandledRejection', bail)
 process.on('uncaughtException', bail)
 
 const app = express()
+app.use(morgan('combined'))
+
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Access-Token, Content-Type, Lang, crossDomain"
+  )
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, OPTIONS, PUT, DELETE"
+  )
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  req.headers.host = req.headers["x-forwarded-host"]
+  res.setHeader("Cache-Control", "no-cache")
+
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.sendStatus(200)
+  } else {
+    //move on
+    next()
+  }
+})
+
 app.use(Gun.serve)
 
 app.get('/', (req, res) => {
